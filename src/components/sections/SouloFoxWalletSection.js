f56976720e668eb2fc4f809180a817f1bbcb1b5b
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-23 23:32:04 -0500

    adding soulfox wallet section

--- src/components/sections/SouloFoxWalletSection.js
+++ src/components/sections/SouloFoxWalletSection.js
@@ -1,13 +1,59 @@
 const SouloFoxWalletSection = () => {
-    return (
-      <section>
-        <div>
-          <h2 className="text-2xl text-200 text-center pt-5 pb-5">
-            SouloFox Wallets
-          </h2>
+  return (
+    <section>
+      <div>
+        <h2 className="text-2xl text-200 text-center pt-5 pb-5">
+          SouloFox Wallets
+        </h2>
+      </div>
+
+      <div className="container">
+        <p>Our wallets will be funded adequately funded as below:</p>
+      </div>
+
+      <div className="container flex ">
+        <div className="w-1/2 ">
+          <p className="">
+            1. Community Wallet (SOLANA balance) 20% NFT sales (buyback floor
+            price/NFT Burn) 2% Secondary NFT sales 40% Community NFT store
+            income
+          </p>
         </div>
-      </section>
-    );  
+
+        <div className="w-1/2 ">
+          <p className="">img</p>
+        </div>
+      </div>
+
+      <div className="container flex ">
+        <div className="w-1/2 ">
+          <p className="">img</p>
+        </div>
+
+        <div className="w-1/2 ">
+          <p className="">
+            2 . Marketing/Partnership wallet (FOXYCOIN balance) 10% Token supply
+            2% P2P marketplace commissions 1% Secondary NFT sales 10% Community
+            NFT store income
+          </p>
+        </div>
+      </div>
+
+      <div className="container flex ">
+        <div className="w-1/2 ">
+          <p className="">
+            3. Game Ecosystem Wallet (FOXYCOIN balance) 8% P2P marketplace
+            commissions 30% Token Supply 10% Community NFT store income 100% In
+            game store income
+          </p>
+        </div>
+
+        <div className="w-1/2 ">
+          <p className="">img</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SouloFoxWalletSection;
